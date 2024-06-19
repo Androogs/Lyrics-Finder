66f860ea3f49f849620b99690359848eb6fee70b
Androogs <139003434+Androogs@users.noreply.github.com>
2024-06-19 12:44:44 -0500

    Finalización de mini-proyecto música

---
+++ js/interfaz.js
@@ -0,0 +1,6 @@
+export const searchForm = document.querySelector('#form-search'),
+    searchDiv = document.querySelector('#search'),
+    messageDiv = document.querySelector('#messages'),
+    resultDiv = document.querySelector('#result'),
+    resultHead = document.querySelector('.letter-result h2');
+
